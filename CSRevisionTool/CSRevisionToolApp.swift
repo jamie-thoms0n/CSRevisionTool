8a60a38dae7c21a4af259538858a3d31b5603fc5
Jamie Thomson <thomson.j@etoncollege.org.uk>
2023-02-02 09:34:19 +0000

    Initial Commit

---
+++ CSRevisionTool/CSRevisionToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSRevisionToolApp.swift
+//  CSRevisionTool
+//
+//  Created by Thomson, Jamie (NA) on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CSRevisionToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSRevisionTool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSRevisionTool
+//
+//  Created by Thomson, Jamie (NA) on 02/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
